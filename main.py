386a6245b1b05ab9e5f2639e82c1e653e4224114
supercrusher <one.incredy@outlook.com>
2023-07-30 17:56:15 +0530

    Data validation & update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -6,6 +7,18 @@ try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nX=======X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    data_Validation = DataValidationTrainingPipeline()
+    data_Validation.main()
     logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nX=======X")
 except Exception as e:
     logger.exception(e)
