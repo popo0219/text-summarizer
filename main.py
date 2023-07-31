8c7d116507b4e1272b959a6f700137b76d3abbf8
supercrusher <one.incredy@outlook.com>
2023-07-31 11:47:59 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -19,6 +20,17 @@ try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_Validation = DataValidationTrainingPipeline()
     data_Validation.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nX=======X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nX=======X")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,8 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
 from textSummarizer.entity import (DataIngestionConfig,
-                                   DataValidationConfig)
+                                   DataValidationConfig,
+                                   DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -45,3 +46,17 @@ class ConfigurationManager:
         )
 
         return data_Validation_config
+    
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
